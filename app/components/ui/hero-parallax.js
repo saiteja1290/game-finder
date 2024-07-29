fdf5ad2ca6297d0f5b0a09c8f2414fad5979a93b
saiteja.palegar@gmail.com <saiteja.palegar@gmail.com>
2024-07-30 00:29:01 +0530

    heroparallazx

--- app/components/ui/hero-parallax.js
+++ app/components/ui/hero-parallax.js
@@ -99,12 +99,11 @@ export const Header = () => {
     return (
         <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
             <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-                The Ultimate <br /> development studio
+                Find People <br /> to play with
             </h1>
             <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-                We build beautiful products with the latest technologies and frameworks.
-                We are a team of passionate developers and designers that love to build
-                amazing products.
+                Find people to play with, join a team, or start your own. <br />
+                We have a community of players waiting for you.
             </p>
         </div>
     );
